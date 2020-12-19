4e54d61d6367e15f1fff952b61f6d07abc1b7907
Kyosuke <kyou0209@gmail.com>
2020-12-19 21:34:18 +0900

    不要ファイル削除

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-var btn = document.getElementById('btn');
- 
-    btn.addEventListener('click', function() {
-      alert(document.myform.mytext.value);
-  
-      //submit()でフォームの内容を送信
-      document.myform.submit();
-    })
